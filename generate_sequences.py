28c3c2247f0b78ddc79cf2efd508fef2c6e4915b
Nilton <niltonlqjr@gmail.com>
2022-12-08 15:50:00 -0300

    generate sequences saving yaml

--- generate_sequences.py
+++ generate_sequences.py
@@ -4,6 +4,7 @@ import random
 import argparse
 import subprocess
 import os
+import yaml as yl
 
 LLVM_FLAGS = [
     '-adce', '-always-inline', '-argpromotion', '-bb-vectorize', '-block-placement',
@@ -22,7 +23,7 @@ LLVM_FLAGS = [
 
 default_len_min = 1
 default_len_max = 37
-default_seqs_total = 100
+default_seqs_total = 5
 
 def can_compile(prog_path, seq_str):
     cmd = 'cd ' + prog_path + ' && ' \
@@ -39,19 +40,19 @@ def can_compile(prog_path, seq_str):
 #    subprocess.run(['opt'] + seq_str + [prog_path, '-o', prog_path + '_o.bc'])
 
 
-def generate_seqs(seed, lmin, lmax, total, prog_path):
+def generate_seqs(seed, lmin, lmax, total, prog_path, flags):
     random.seed(seed)
     generated_seqs = dict()
     while len(generated_seqs) < total:
         N = random.randint(lmin, lmax)
-        new_seq = dict() # Don't repeat the flags.
+        new_seq = []
         while len(new_seq) < N:
-            new_seq[LLVM_FLAGS[random.randint(0, len(LLVM_FLAGS) - 1)]] = None
+            new_seq.append(flags[random.randint(0, len(flags) - 1)])
 
-        seq_str = ''
-        for seq in new_seq: # Concatenate seqs from dict into single string.
-            seq_str += ' ' + seq
-        seq_str += ' '
+        seq_str = ' '.join(new_seq)
+        #for seq in new_seq: # Concatenate seqs from dict into single string.
+        #    seq_str += ' ' + seq
+        #seq_str += ' '
 
         # seq_str = []
         # for seq in new_seq: # Concatenate seqs from dict into single string.
@@ -59,21 +60,43 @@ def generate_seqs(seed, lmin, lmax, total, prog_path):
 
         if can_compile(prog_path, seq_str): # Some sequences may brake a program.
             generated_seqs[seq_str] = None # Add new generated sequence.
+    ret=dict()
+    i=0
     for seq in generated_seqs.keys(): # Print generated sequences to stdout.
-        print(seq)
+        ret[i] = seq
+        i+=1
+    return ret
 
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s [--min VAL] [--max VAL] [--seed VAL] [--total VAL] --prog PATH_TO_BENCHMARK",
+        usage="%(prog)s [--min VAL] [--max VAL] [--seed VAL] [--total VAL] [--flags-file PATH_TO_FILE] --prog PATH_TO_BENCHMARK",
         description="Generate random sequences of optimizations."
     )
     parser.add_argument('--min', dest='len_min', default=default_len_min)
     parser.add_argument('--max', dest='len_max', default=default_len_max)
     parser.add_argument('--total', dest='total', default=default_seqs_total)
     parser.add_argument('--prog', dest='prog', required=True)
+    parser.add_argument('--flags-file', dest='flags_file', default=None)
+    parser.add_argument('--output', '-o', dest='output', default='output.yaml')
     parser.add_argument('--seed', dest='seed')
     return parser
 
 args = init_argparse().parse_args()
-generate_seqs(args.seed, int(args.len_min), int(args.len_max), int(args.total), args.prog)
+
+if args.flags_file != None:
+    with open(args.flags_file) as f:
+        flags = yl.safe_load(f)
+else:
+    flags = LLVM_FLAGS
+
+print(flags)
+seqs = generate_seqs(args.seed, int(args.len_min), int(args.len_max), int(args.total), args.prog,flags)
+data=dict()
+data['metodology'] = dict()
+data['metodology']['minimum sequence length'] = args.len_min
+data['metodology']['maximum sequence length'] = args.len_max
+data['metodology']['total sequences'] = args.total
+data['sequences']=seqs
+with open(args.output,'w') as f:
+    yl.dump(data,f)
