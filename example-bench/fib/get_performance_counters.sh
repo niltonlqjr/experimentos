b5417b42bbcd6c83b03011137139b7958497498d
Nilton <niltonlqjr@gmail.com>
2022-12-19 20:01:48 -0300

    example bench perf script

--- example-bench/fib/get_performance_counters.sh
+++ example-bench/fib/get_performance_counters.sh
@@ -1,12 +1,13 @@
 events=$1 #'instructions:u,ref-cycles:u'
-workset=$2 #'1000'
+workset=$2 #'test'
 report_name=$3 #'performance_counters.csv'
  
 case ${workset} in 
 'test')
     args=1000
-    input=/dev/null
     output='output.txt'
+    err='error.txt'
+    work_dir="../"
     ;;
 *)
     echo "invalid workset"
@@ -14,4 +15,14 @@ case ${workset} in
     ;;
 esac
 
-perf stat -e ${events} -x, -o ${report_name} ./a.out ${args} < ${input} > ${output}
+cp a.out ${work_dir}
+cur_dir=`pwd`
+
+cd ${work_dir}
+perf stat -e ${events} -x, -o ${report_name} ./a.out ${args} > ${output} 2> ${err}
+
+rm ${output}
+rm ${err}
+rm a.out
+cd ${cur_dir}
+mv ${work_dir}/${report_name} ${report_name}
