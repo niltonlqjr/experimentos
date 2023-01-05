a4b95bc122a3f302ac619cfea635a928289f7af5
Nilton <niltonlqjr@gmail.com>
2023-01-05 17:05:06 -0300

    script pcs spec 2006

---
+++ scripts/3.0.extract_pcs_cpu06.sh
@@ -0,0 +1,22 @@
+#benchmarks=('astar' 'dealII' 'gobmk' 'hmmer' 'libquantum' 'milc' 'omnetpp' 'povray' 'bzip2' 'gcc' 'h264ref' 'lbm' 'mcf' 'namd' 'sjeng')
+benchmarks=('astar' 'gobmk' 'hmmer' 'milc' 'omnetpp' 'povray' 'bzip2' 'gcc' 'h264ref' 'lbm' 'mcf' 'namd' 'sjeng')
+benchmarks_dir=../benchmarks/cpu2006
+
+script_dir=../extract-performance-counters
+sequences_dir=../results/sequences/spec2006
+output_dir=../results/PCs/spec2006
+log_dir=../logs/PCs/spec2006
+
+PCs_files='PCs_hardware_26.yaml'
+
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
+
+for b in ${benchmarks[@]}
+do
+    echo $b
+    python3 ${script_dir}/extract_pcs.py ${benchmarks_dir}/${b}/src \
+            ${sequences_dir}/${b}.yaml -f $PCs_files -r 10 \
+            -o=${output_dir}/${b}.yaml > ${log_dir}/${b}.out \
+            2> ${log_dir}/${b}.err
+done
